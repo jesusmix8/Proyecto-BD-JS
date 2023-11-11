96a8c9816bcaa1717f2fe41895e3a39764294133
Jesus Sanchez <jesusmix8@icloud.com>
2023-11-11 17:17:50 -0600

    Remove commented out code and add TODO for transfer implementation

---
+++ db/triggers.sql
@@ -0,0 +1,19 @@
+CREATE OR REPLACE FUNCTION crear_cuenta_despues_insertar_cliente()
+RETURNS TRIGGER AS $$
+BEGIN
+    -- Insertar una nueva fila en la tabla cuenta
+    INSERT INTO cuenta (cliente_ID) VALUES (NEW.cliente_ID);
+
+    -- Puedes establecer un saldo predeterminado de 0 pesos
+    UPDATE cuenta SET saldo = 0 WHERE cliente_ID = NEW.cliente_ID;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Crear el trigger
+CREATE TRIGGER trigger_crear_cuenta
+AFTER INSERT
+ON cliente
+FOR EACH ROW
+EXECUTE FUNCTION crear_cuenta_despues_insertar_cliente();
--- src/controllers/client.controler.js
+++ src/controllers/client.controler.js
@@ -137,13 +137,6 @@ const getDataClient = async (req, res) => {
 const loaddashboard = async (req, res) => {
   const usuario = req.session.usuario;
   if (usuario) {
-    // const saldo = await pool.query(
-    //   "Select saldo from cuenta where usuario = $1",
-    //   [usuario[0].usuario]
-    // );
-    // delete usuario[0].contraseña;
-    // usuario[0].saldo = saldo.rows[0].saldo;
-    // console.log(usuario);
     res.render("dashboard", { usuario: usuario });
   } else {
     res.json({ message: "No hay usuario en la sesión" });
@@ -160,48 +153,58 @@ const cargadePantallaTransferencia = (req, res) => {
     res.redirect("/login");
   }
 };
+//TODO implementar la transferencia
+//Aqui se recibe el usuario de la cuenta destino, el monto y la descripcion desde el html "formtransferencia.html"
+//Usando los datos de la sesion en req.session.usuario se obtiene el usuario de la cuenta origen
+//Se comprueba si existe una cuenta destino con ese usuario
+//Se comprueba si la cuenta origen y destino son la misma
+//Se comprueba si la cuenta origen tiene saldo suficiente para realizar la transferencia
+//Se realiza la transferencia
 
 const transferclient = async (req, res) => {
-  const { cuentaDestino, monto, descripcion } = req.body;
-  console.log(cuentaDestino, monto, descripcion);
-  //Comprobar si existe una cuenta destino con ese usuario
-  const result = await pool.query(
-    "SELECT id_cuenta FROM cuenta WHERE usuario = $1",
-    [cuentaDestino]
-  );
 
-  if (result.rows.length > 0) {
-    //Comprobar si la cuenta origen y destino son la misma
-    console.log("Cuenta destino encontrada");
-    const usuario = req.session.usuario;
-    const cuentaOrigen = usuario[0].usuario;
-    const ID_Cuenta = usuario[0].id_cuenta;
-    const ID_CuentaDestino = result.rows[0].id_cuenta;
-    if (ID_Cuenta === result.rows[0].id_cuenta) {
-      res
-        .status(400)
-        .json({ message: "No puedes transferir a la misma cuenta" });
-    } else {
-      //Comprobar si la cuenta origen tiene saldo suficiente para realizar la transferencia
-      const saldoOrigen = await pool.query(
-        "Select saldo from cuenta where usuario = $1",
-        [cuentaOrigen]
-      );
+  console.log("Ejecutando desde transferclient")
+    
+  // const { cuentaDestino, monto, descripcion } = req.body;
+  // console.log(cuentaDestino, monto, descripcion);
+  // //Comprobar si existe una cuenta destino con ese usuario
+  // const result = await pool.query(
+  //   "SELECT id_cuenta FROM cuenta WHERE usuario = $1",
+  //   [cuentaDestino]
+  // );
 
-      const montoTransferencia = parseInt(monto);
-      if (saldoOrigen < montoTransferencia) {
-        res.status(400).json({ message: "Saldo insuficiente" });
-      } else {
-        const transferenciaQuery = await pool.query(
-          "INSERT INTO Transaccion (Tipo_Transaccion, Monto, Fecha_Hora, ID_Cuenta, CuentaDestino) VALUES ($1, $2, CURRENT_TIMESTAMP, $3, $4)",
-          ["Transferencia", montoTransferencia, ID_Cuenta, ID_CuentaDestino]
-        );
-        res.status(200).json({ message: "Transferencia exitosa" });
-      }
-    }
-  } else {
-    res.status(404).json({ message: "Cuenta destino no encontrada" });
-  }
+  // if (result.rows.length > 0) {
+  //   //Comprobar si la cuenta origen y destino son la misma
+  //   console.log("Cuenta destino encontrada");
+  //   const usuario = req.session.usuario;
+  //   const cuentaOrigen = usuario[0].usuario;
+  //   const ID_Cuenta = usuario[0].id_cuenta;
+  //   const ID_CuentaDestino = result.rows[0].id_cuenta;
+  //   if (ID_Cuenta === result.rows[0].id_cuenta) {
+  //     res
+  //       .status(400)
+  //       .json({ message: "No puedes transferir a la misma cuenta" });
+  //   } else {
+  //     //Comprobar si la cuenta origen tiene saldo suficiente para realizar la transferencia
+  //     const saldoOrigen = await pool.query(
+  //       "Select saldo from cuenta where usuario = $1",
+  //       [cuentaOrigen]
+  //     );
+
+  //     const montoTransferencia = parseInt(monto);
+  //     if (saldoOrigen < montoTransferencia) {
+  //       res.status(400).json({ message: "Saldo insuficiente" });
+  //     } else {
+  //       const transferenciaQuery = await pool.query(
+  //         "INSERT INTO Transaccion (Tipo_Transaccion, Monto, Fecha_Hora, ID_Cuenta, CuentaDestino) VALUES ($1, $2, CURRENT_TIMESTAMP, $3, $4)",
+  //         ["Transferencia", montoTransferencia, ID_Cuenta, ID_CuentaDestino]
+  //       );
+  //       res.status(200).json({ message: "Transferencia exitosa" });
+  //     }
+  //   }
+  // } else {
+  //   res.status(404).json({ message: "Cuenta destino no encontrada" });
+  // }
 };
 
 const pantalladeahorro = (req, res) => {
