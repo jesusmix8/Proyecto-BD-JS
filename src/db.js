74560aa5a9a2b77dd98b9c406c4b5bf0aa1d21a6
Jesus Sanchez <jesusmix8@icloud.com>
2023-11-14 17:53:24 -0600

    Update .gitignore and db.js

--- src/db.js
+++ src/db.js
@@ -3,8 +3,8 @@ const {Pool} = require('pg');
 const pool = new Pool({
     user: "postgres",
     host: "localhost",
-    password: "Yeiglo20",
-    database: "BancoDb",
+    password: "admin",
+    database: "Banco",
     port: "5432"
 });
 
