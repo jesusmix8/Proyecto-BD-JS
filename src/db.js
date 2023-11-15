efe8855d853bdccdcc284d8b30d1e0c86dcf71d3
PepePecas <wilzonneta@gmail.com>
2023-11-14 20:02:24 -0600

    contrasena

--- src/db.js
+++ src/db.js
@@ -3,8 +3,8 @@ const {Pool} = require('pg');
 const pool = new Pool({
     user: "postgres",
     host: "localhost",
-    password: "admin",
-    database: "Banco",
+    password: "BasesDeDatos",
+    database: "banco",
     port: "5432"
 });
 
