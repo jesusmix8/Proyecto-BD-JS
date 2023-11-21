1c0891b1af16ac0e7a775232c24b8e4772787065
Jesus Sanchez <jesusmix8@icloud.com>
2023-11-21 08:36:48 -0600

    Añadi rutas al cliente

--- src/routes/client.routes.js
+++ src/routes/client.routes.js
@@ -1,62 +1,70 @@
-const {Router} = require('express');
+const { Router } = require("express");
 const router = Router();
-const { inicio, FormNewClient, createClient, login, getDataClient, loaddashboard , logout, 
-    cargadePantallaTransferencia, realizarTransferenciaCliente , SolicitudDeTdc, 
-    crearTDC, pantallatdc, SolicitudDeSeguro, crearSeguro, pantallaseguro, SolicitudDePrestamo,
-    crearPrestamo, pantallaprestamo, SolicitudDeAhorro, crearAhorro, pantalladeahorro,
-    pantallaDeposito, realizarDeposito} 
-    = require('../controllers/client.controler');
-
-
+const {
+  inicio,
+  FormNewClient,
+  createClient,
+  login,
+  getDataClient,
+  loaddashboard,
+  logout,
+  cargadePantallaTransferencia,
+  realizarTransferenciaCliente,
+  SolicitudDeTdc,
+  crearTDC,
+  pantallatdc,
+  SolicitudDeSeguro,
+  crearSeguro,
+  pantallaseguro,
+  SolicitudDePrestamo,
+  crearPrestamo,
+  pantallaprestamo,
+  SolicitudDeAhorro,
+  crearAhorro,
+  pantalladeahorro,
+  pantallaDeposito,
+  realizarDeposito,
+} = require("../controllers/client.controler");
 
 //Ruta inicial para cliente
-router.get('/', inicio);
+router.get("/", inicio);
 
 //Ruta para la creacion de cliente
-router.get ('/nuevocliente' , FormNewClient);
-router.post('/client_register',createClient );
+router.get("/nuevocliente", FormNewClient);
+router.post("/client_register", createClient);
 //Rutas relacionadas para la autenticacion de cliente
-router.get('/login' , login);
-router.post('/login',getDataClient );
-router.get('/perfil', loaddashboard);
-router.get('/logout', logout);
-
-
+router.get("/login", login);
+router.post("/login", getDataClient);
+router.get("/perfil", loaddashboard);
+router.get("/logout", logout);
 
 //Rutas para servicios de cliente
-router.get('/transferencia', cargadePantallaTransferencia)
-router.post('/transfer', realizarTransferenciaCliente)
+router.get("/transferencia", cargadePantallaTransferencia);
+router.post("/transfer", realizarTransferenciaCliente);
 
 //Tarjeta de credito
-router.get('/solicitarTDC', SolicitudDeTdc)
-router.post('/solicitudTDC', crearTDC)
-router.get('/tdc', pantallatdc)
+router.get("/solicitarTDC", SolicitudDeTdc);
+router.post("/solicitudTDC", crearTDC);
+router.get("/tdcDetalles", pantallatdc);
 
 //Seguro
-router.get('/Seguro', SolicitudDeSeguro)
-router.post('/solicitudSeguro', crearSeguro)
-router.get('/seguro', pantallaseguro)
+router.get("/Seguro", SolicitudDeSeguro);
+router.post("/solicitudSeguro", crearSeguro);
+router.get("/seguroDetalle", pantallaseguro);
 
 //prestamo
-router.get('/prestamo', SolicitudDePrestamo)
-router.post('/solicitudPrestamo', crearPrestamo)
-router.get('/prestamo', pantallaprestamo)
+router.get("/prestamo", SolicitudDePrestamo);
+router.post("/solicitudPrestamo", crearPrestamo);
+router.get("/prestamoDetalle", pantallaprestamo);
 
 //Ahorro
-router.get('/ahorro', SolicitudDeAhorro)
-router.post('/crearAhorro', crearAhorro)
-router.get('/ahorro', pantalladeahorro)
+router.get("/ahorro", SolicitudDeAhorro);
+router.post("/crearAhorro", crearAhorro);
+router.get("/ahorroDetalle", pantalladeahorro);
 
 //deposito
-router.get('/depositar',pantallaDeposito)
-router.post('/deposito',realizarDeposito)
-
-
-
-
-
-
-
+router.get("/depositar", pantallaDeposito);
+router.post("/deposito", realizarDeposito);
 
 //router.delete('/client', deleteClient);
 
