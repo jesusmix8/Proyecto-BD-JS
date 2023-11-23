e6ef835caba356bb473e7495cf8500ef781bdb73
Jesus Sanchez <jesusmix8@icloud.com>
2023-11-23 10:08:53 -0600

    Refactor code formatting and file structure

--- src/index.js
+++ src/index.js
@@ -1,46 +1,41 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 const app = express();
-const crypto = require('crypto');
+const crypto = require("crypto");
 
 const generateRandomKey = (length) => {
-    return crypto.randomBytes(Math.ceil(length / 2)).toString('hex')
-}
+  return crypto.randomBytes(Math.ceil(length / 2)).toString("hex");
+};
 
-const cors = require('cors');
-const path = require('path');
-const session = require('express-session');
+const path = require("path");
 
-app.set('view engine', 'ejs');
+const session = require("express-session");
 
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
 
+app.set("views", path.join(__dirname, "views"));
 
+const routesClient = require("./routes/client.routes");
 
-const routesClient = require('./routes/client.routes');
-const routesEmployee = require('./routes/employee.routes');
+const routesEmployee = require("./routes/employee.routes");
 
-app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
-app.use(express.static('src'));
+app.use(express.static("src"));
 
+app.use("/static", express.static(path.join(__dirname, "views")));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/static', express.static(path.join(__dirname, 'views')));
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-app.use(session({
-    secret: 'generateRandomKey(64)',
+app.use(
+  session({
+    secret: "generateRandomKey(64)",
     resave: true,
-    saveUninitialized: true
-}));
-
+    saveUninitialized: true,
+  })
+);
 
 app.use(routesClient);
 app.use(routesEmployee);
 
-
-
 app.listen(3000);
-console.log('Server on port', 3000);
+console.log("Server on port", 3000);
