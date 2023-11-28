7eba2d3c48eb58c3a9eab8d818e23489ac54933a
esdrazz <esdrasvictoria123@gmail.com>
2023-11-27 18:21:18 -0600

    He modificado la clientControler

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 const pool = new Pool({
   user: "postgres",
   host: "localhost",
-  password: "EsdrasUn698",
+  password: "admin",
   database: "banco",
   port: "5432",
 });
