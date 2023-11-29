3b30807b237d6455f36696755be2ed3c8430fa37
esdrazz <esdrasvictoria123@gmail.com>
2023-11-28 21:56:36 -0600

    He anadido crearAhorro

--- src/views/Limite/limite.js
+++ src/views/Limite/limite.js
@@ -1,35 +1,39 @@
+const mensajeAhorro = document.getElementById("mensajeAhorro");
 
-const mensaje = document.getElementById('mensaje')
-
-
-document.getElementById('form-ahorro').addEventListener('submit', function (e) {
+document.getElementById("form-ahorro").addEventListener("submit", function (e) {
     e.preventDefault(); // Evitar la recarga de la página
 
-    const NombreDelAhorro = document.getElementById('NombreDelAhorro').value;
-    const Plazo = document.getElementById('plazos').value;
-    const monto = document.getElementById('monto').value;
-
-
-
-
+    const nombreDelAhorro = document.getElementById("NombreDelAhorro").value;
+    const plazo = document.getElementById("plazos").value;
+    const monto = document.getElementById("monto").value;
 
     const data = {
-        NombreDelAhorro: NombreDelAhorro,
-         Plazo: Plazo,
-         Monto: monto
+        nombreDelAhorro: nombreDelAhorro,
+        plazo: plazo,
+        monto: monto
     };
 
-     fetch('/crearAhorro', {
-         method: 'POST',
-             headers: {
-             'Content-Type': 'application/json'
-         },
-         body: JSON.stringify(data)
-     })
-     .then(response => {
-         if (response.status === 200) {
-            mensaje.textContent = 'Cuenta registrada exitosamente';
-            mensaje.style.display = 'block'; // Mostrar mensaje de éxito
+    fetch('/crearAhorro', {
+        method: 'POST',
+            headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(response => {
+        if (response.status === 200) {
+            mensajeAhorro.textContent = 'Ahorro creado exitosamente';
+            mensajeAhorro.style.display = 'block'; // Mostrar mensaje de éxito
+
+            setTimeout(function(){
+                window.location.href = "/perfil";
+            }, 2000)
+        }else if(response.status === 400){
+            return response.json().then((data) => {
+                mensajeAhorro.textContent = data.message;
+                mensajeAhorro.style.display = 'block'; // Mostrar mensaje de error
+            });
+        }else if(response.status === 404){
+            mensajeAhorro.textContent = 'Error desconocido';
         }
-     });
+    });
 });
