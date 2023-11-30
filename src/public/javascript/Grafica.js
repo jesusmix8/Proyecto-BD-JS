00c365bb72bd210b1fabc1c7d554a8040caf4e1e
Yeiser20 <archundiadaniel@17gmail.com>
2023-11-29 23:01:58 -0600

    cambiosDashboardEmployee

---
+++ src/public/javascript/Grafica.js
@@ -0,0 +1,61 @@
+    const inputFecha = document.getElementById('inputFecha');
+    const canvas = document.getElementById('grafica');
+    const ctx = canvas.getContext('2d');
+  
+      async function cargarJSON() {
+        try {
+          const respuesta = await fetch('/html/resultado.json');
+  
+          if (!respuesta.ok) {
+            throw new Error('Error al cargar el archivo JSON');
+          }
+          const datosJson = await respuesta.json();
+          function actualizarGrafica() {
+            const fechaSeleccionada = inputFecha.valueAsDate;
+            if (!fechaSeleccionada) return;
+  
+            const mesSeleccionado = (fechaSeleccionada.getMonth() + 1).toString().padStart(2, '0');
+            const anoSeleccionado = fechaSeleccionada.getFullYear().toString();
+  
+            const datosFiltrados = datosJson.filter(dato => {
+              const fechaDato = dato.mes;
+              return fechaDato && fechaDato.startsWith(`${anoSeleccionado}-${mesSeleccionado}`);
+            });
+  
+            dibujarGrafica(datosFiltrados);
+          }
+          inputFecha.addEventListener('change', actualizarGrafica);
+  
+          actualizarGrafica();
+        } catch (error) {
+          console.error('Error al cargar el archivo JSON:', error.message);
+        }
+      }
+  
+      function dibujarGrafica(datos) {
+        const etiquetas = datos.map(dato => dato.sucursal);
+        const datosTransferencias = datos.map(dato => +dato.numerodetransferencias);
+
+        const grafica = new Chart(ctx, {
+          type: 'bar',
+          data: {
+            labels: etiquetas,
+            datasets: [{
+              label: 'Número de Transferencias',
+              data: datosTransferencias,
+              backgroundColor: 'rgba(75, 192, 192, 0.2)',
+              borderColor: 'rgba(75, 192, 192, 1)',
+              borderWidth: 1
+            }]
+          },
+          options: {
+            scales: {
+              y: {
+                beginAtZero: true
+              }
+            }
+          }
+        });
+      }
+
+      cargarJSON();
